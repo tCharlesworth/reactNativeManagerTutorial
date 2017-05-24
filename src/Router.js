e6f0051a3e8a097c5b885ecd5bab4f2c7a3eca51
Tim Charlesworth <tim_charlesworth@outlook.com>
2017-05-24 15:43:11 -0600

    Create employeeList component Added the employee list to the navigation flow and filled it with some temporary dummy data so there is something to see rendered.

--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Scene, Router } from 'react-native-router-flux';
 
-import LoginForm from './components/LoginForm';
+import LoginForm    from './components/LoginForm';
+import EmployeeList from './components/EmployeeList';
 
 const RouterComponent = () => {
   return (
     <Router sceneStyle={{ paddingTop: 65 }}>
-      <Scene key="login" component={LoginForm} title="Login" />
+      <Scene key="auth">
+        <Scene key="login" component={LoginForm} title="Login" />
+      </Scene>
+      <Scene key="main">
+        <Scene key="employeeList" component={EmployeeList} title="Employees" />
+      </Scene>
     </Router>
   )
 
--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 import { EMAIL_CHANGED, PASSWORD_CHANGED, LOGIN_USER_SUCCESS, LOGIN_USER_FAILED, LOGIN_USER_STARTED } from './types';
+import { Actions } from 'react-native-router-flux';
 import firebase from 'firebase';
 
 export const emailChanged = (email) => {
@@ -33,6 +34,8 @@ const loginUserSuccess = (dispatch, user) => {
         type: LOGIN_USER_SUCCESS,
         payload: user
     });
+
+    Actions.main();
 };
 
 const loginUserFailure = (dispatch) => {
